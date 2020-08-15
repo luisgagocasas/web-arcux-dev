4fd404a9de526703d01352419b7ec0161dbf9c5c
Luis Gago Casas <luisgago@lagc-peru.com>
2020-08-15 15:23:26 -0500

    update init

---
+++ build/31d49d87.1a42f02c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{65:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hola","name":"hola","count":2,"permalink":"/blog/tags/hola"}')}}]);
